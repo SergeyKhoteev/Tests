352ca95e108ade039686e8c5882dc910f92ef1ab
SergeyKhoteev <khoteev.s@gmail.com>
2022-07-29 09:59:42 +0300

    Commit name - Login tests integrated
    Why - test script covers both pages
    Changes - in test_main_page loginpage initialized

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
 		self.should_be_register_form()
 
 	def should_be_login_url(self):
-		assert 'login' in browser.current_url, 'login not in link'
+		assert 'login' in self.browser.current_url, 'login not in link'
 
 	def should_be_login_form(self):
 		assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'login_form is not presented'
